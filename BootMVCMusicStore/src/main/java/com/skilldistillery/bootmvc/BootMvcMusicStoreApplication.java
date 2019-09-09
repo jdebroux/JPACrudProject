7c0a9c17b02e26c34024d8b5fe077100f3f72a42
JoeDeBruh <joedebruh@braidens-MacBook-Pro.local>
2019-09-09 09:54:34 -0600

    finished plus war and tomcat information

--- BootMVCMusicStore/src/main/java/com/skilldistillery/bootmvc/BootMvcMusicStoreApplication.java
+++ BootMVCMusicStore/src/main/java/com/skilldistillery/bootmvc/BootMvcMusicStoreApplication.java
@@ -3,13 +3,19 @@ package com.skilldistillery.bootmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.music.entities")
-public class BootMvcMusicStoreApplication {
+public class BootMvcMusicStoreApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMvcMusicStoreApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(BootMvcMusicStoreApplication.class);
+	}
 }
